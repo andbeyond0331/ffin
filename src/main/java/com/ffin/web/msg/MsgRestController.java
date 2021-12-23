2d5be04852a1aaa2071359982ecce25e3325e90e
HHJ <florahhj@daum.net>
2021-12-23 10:01:04 +0900

    21.12.23 HHJ: viewResolver에 prefix 추가, message 부분 수정

--- src/main/java/com/ffin/web/msg/MsgRestController.java
+++ src/main/java/com/ffin/web/msg/MsgRestController.java
@@ -1,71 +1,59 @@
 package com.ffin.web.msg;
 
 
+
+import com.ffin.service.catering.CateringService;
+import com.ffin.service.domain.Catering;
 import com.ffin.service.domain.Msg;
 import com.ffin.service.msg.MsgService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Handles requests for the application home page.
- */
-@Controller
+@RestController
 @RequestMapping("/msg/*")
-public class MsgController {
+public class MsgRestController {
 
+    ///Field
     @Autowired
     @Qualifier("msgServiceImpl")
     private MsgService msgService;
 
-    public MsgController(){
+
+    public MsgRestController(){
         System.out.println(this.getClass());
     }
 
-
-
-    // 메세지 목록
-    @RequestMapping(value = "/message_list")
-    public ModelAndView message_list(HttpServletRequest request, HttpSession session) throws Exception {
-        // System.out.println("현대 사용자 nick : " + session.getAttribute("nick"));
-        System.out.println("MsgController.message_list");
-        //test하느라 id값 고정으로 해놓음
-        //String id = (String) session.getAttribute("userId");
-        String id="user01";
-
-        Msg to = new Msg();
-        to.setId(id);
-
-        // 메세지 리스트
-       // ArrayList<Msg> list = msgService.messageList(to);
-        List<Msg> list = msgService.messageList(to);
-
-        request.setAttribute("list", list);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("list", list);
-        modelAndView.setViewName("/msg/message_list.jsp");
-        return modelAndView;
-    }
 /*
 
-    // 메세지 목록
-    @RequestMapping(value = "/message_ajax_list")
-    public ModelAndView message_ajax_list(HttpServletRequest request, HttpSession session) throws Exception {
-        // System.out.println("현대 사용자 nick : " + session.getAttribute("nick"));
-        System.out.println("MsgController.message_ajax_list");
+    @Value("#{commonProperties['pageUnit']}")
+    //@Value("#{commonProperties['pageUnit'] ?: 3}")
+    int pageUnit;
 
+    @Value("#{commonProperties['pageSize']}")
+    //@Value("#{commonProperties['pageSize'] ?: 2}")
+    int pageSize;
+*/
+
+
+    @RequestMapping( value="json/message_ajax_list")
+    @ResponseBody
+    public ModelAndView message_ajax_list(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        System.out.println("MsgController.message_ajax_list");
+        request.setCharacterEncoding("UTF-8");
         //String id = (String) session.getAttribute("userId");
-        String id="user03";
+        String id="user01";
 
         Msg to = new Msg();
         to.setId(id);
@@ -75,58 +63,68 @@ public class MsgController {
         //ArrayList<Msg> list = msgService.messageList(to);
         List<Msg> list = msgService.messageList(to);
 
-       // request.setAttribute("list", list);
+         request.setAttribute("list", list);
 
-       // return "forward: cmnt/message_ajax_list";
+        // return "/message_ajax_list.jsp";
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("list", list);
         modelAndView.setViewName("/WEB-INF/views/msg/message_ajax_list.jsp");
         return modelAndView;
+        //return list;
     }
 
-    @RequestMapping(value = "/message_content_list")
+
+    @RequestMapping(value = "json/message_content_list")
     public ModelAndView message_content_list(HttpServletRequest request, HttpSession session) throws Exception {
+        request.setCharacterEncoding("UTF-8");
         System.out.println("MsgController.message_content_list");
-        //int msgRoom = Integer.parseInt(request.getParameter("msgRoom"));
+       // int msgRoom = Integer.parseInt(request.getParameter("msgRoom"));
         int msgRoom = 1;
         Msg to = new Msg();
         to.setMsgRoom(msgRoom);
-       // to.setId((String) session.getAttribute("userId"));
-to.setId("user03");
+        // to.setId((String) session.getAttribute("userId"));
+        to.setId("user01");
 
         // 메세지 내용을 가져온다.
         ArrayList<Msg> clist = msgService.roomContentList(to);
-        System.out.println("clist: "+clist);
-      */
-/*  request.setAttribute("clist", clist);
-
-        return "forward : cmnt/message_content_list";*//*
-
+        request.setAttribute("clist", clist);
+        //return "/WEB-INF/views/msg/message_content_list.jsp";
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("clist", clist);
         modelAndView.setViewName("/WEB-INF/views/msg/message_content_list.jsp");
         return modelAndView;
+        //return clist;
     }
 
     // 메세지 리스트에서 메세지 보내기
     @ResponseBody
-    @RequestMapping(value = "/message_send_inlist")
+    @RequestMapping(value = "json/message_send_inlist")
     public int message_send_inlist(@RequestParam int msgRoom, @RequestParam String other_id,
                                    @RequestParam String msgContent, HttpSession session, HttpServletRequest request) throws Exception {
         request.setCharacterEncoding("UTF-8");
+        msgRoom = Integer.parseInt(request.getParameter("msgRoom"));
+        other_id = request.getParameter("other_id");
+        msgContent = request.getParameter("msgContent");
+        System.out.println("=========================\n msgContent = " + msgContent);
 
         Msg to = new Msg();
         to.setMsgRoom(msgRoom);
-       // to.setMsgSendUserId((String) session.getAttribute("UserId"));
-        to.setMsgSendUserId("user03");
+        // to.setMsgSendUserId((String) session.getAttribute("UserId"));
+        to.setMsgSendUserId("user01");
         to.setMsgRecvUserId(other_id);
         to.setMsgContent(msgContent);
-        System.out.println("msgRoom = " + msgRoom + ", other_id = " + other_id + ", msgContent = " + msgContent + ", session = " + session);
-        
+        //System.out.println("msgRoom = " + msgRoom + ", other_id = " + other_id + ", msgContent = " + msgContent + ", session = " + session);
+
         int flag = msgService.messageSendInlist(to);
         System.out.println("flag = " + flag);
         return flag;
     }
-*/
+
+
+
+
+
+
+
 
 }
